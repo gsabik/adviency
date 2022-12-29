8d81d1bf57582199c96f087221619c42d233422e
Guido Sabik <guido.sabik@gmail.com>
2022-12-29 17:54:25 -0300

    create theme and add font

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./theme";
 import AdviencyApp from "./AdviencyApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<ChakraProvider>
+	<ChakraProvider theme={theme}>
 		<AdviencyApp/>
 	</ChakraProvider>
 );
